4aeb9e48fc1b51448e0bbc177ca4f9392fef2dc9
Fredy Camas <fredycamasdev@gmail.com>
2023-10-28 13:21:14 -0400

    added ground programmatically

--- Balloid/GameScene.swift
+++ Balloid/GameScene.swift
@@ -23,13 +23,27 @@ class GameScene: SKScene {
 extension GameScene {
     func setUpNodes(){
         createBG()
+        createLand()
     }
     
     func createBG(){
         let bg = SKSpriteNode(imageNamed: "bg")
         bg.anchorPoint = .zero
         bg.position = .zero
-        bg.zPosition = 1.0
+        bg.zPosition = -3.0
         addChild(bg)
     }
+    
+    func createLand() {
+        for i in 0...2 {
+            let ground = SKSpriteNode(imageNamed: "land-ic")
+            ground.name = "Land"
+            ground.xScale = CGFloat(1)
+            ground.anchorPoint = .zero
+            ground.zPosition = 1.0
+            ground.position = CGPoint(x: CGFloat(i)*ground.frame.width , y: 200.0)
+            addChild(ground)
+        }
+    }
 }
+
